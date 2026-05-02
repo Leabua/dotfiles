d37f6d5759df9e4d273b95ce8feec9dc496d5500
Leabua Khomari <leabuakhomari@gmail.com>
2026-05-03 01:27:55 +0200

    added backspace delete and silence visual select up and down line movement in keymaps lua nvim

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,22 +1,25 @@
 -- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Default keymaps that are always set: https://github.com/LazyLazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
--- The format is vim.keymap.set("mode", "bind", "what vim must do")
--- This remap is purely so i can move things up and down using the move command (:m) - @thePrimeagen
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- capital J for shift
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- capital K for shift
+-- The format is keymap.set("mode", "bind", "what vim must do")
+local keymap = vim.keymap
+keymap.set("v", "J", ":m '>+1<CR>gv=gv", { silent = true })
+keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
 
 -- Half page jumping with cursor in the same place
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- when using "/" to search this keeps the search term in the middle
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
 
--- This is to stop vim adding replaced text into clipboard
-vim.keymap.set("x", "<leader>p", "\"_dP")
+-- This is to stop adding replaced text into clipboard
+keymap.set("x", "<leader>p", '"_dP')
 
 -- Pressing 'K' in insert mode will show signature help
-vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, { desc = "Signature Help" })
+keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, { desc = "Signature Help" })
+
+-- Delete a word backwards
+keymap.set("i", "<C-h>", "<C-w>")
+keymap.set("n", "<C-h>", "bdw")
