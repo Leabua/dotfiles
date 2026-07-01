3dfd8b0ae71b49c371b32a5cc4bdcad256acf200
Leabua <leabuakhomari@gmail.com>
2026-07-01 17:22:28 +0200

    upfated colors, added marks.nvim and move tmuxNav config into oneshots too

--- nvim/.config/nvim/lua/plugins/oneshot.lua
+++ nvim/.config/nvim/lua/plugins/oneshot.lua
@@ -3,29 +3,52 @@ return {
 		"windwp/nvim-autopairs",
 		event = "InsertEnter",
 		config = true,
-		opts = { enable_check_bracket_line = false, check_ts = true },
+		opts = {
+			enable_check_bracket_line = false,
+			check_ts = true,
+		},
 	},
 	{
 		"lewis6991/gitsigns.nvim",
 		event = { "BufReadPre", "BufNewFile" },
 		opts = {},
 	},
-	-- {
-	--   "nvim-mini/mini"
-	--   --mini surround, mini ai, mini, mini_icons - kickstart
-	-- },
 	{
 		"j-hui/fidget.nvim",
 		event = "LspAttach",
 		opts = {
 			progress = {
-				suppress_on_insert = true, -- don't show messages while in insert mode
-				ignore_done_already = true, -- ignore tasks that are already complete
+				suppress_on_insert = true,
+				ignore_done_already = true,
 				display = {
 					render_limit = 1, -- max number of messages shown at once
-					done_ttl = 3, -- how long a finished message lingers (seconds)
+					done_ttl = 3, -- how long a finished message lingers in seconds
 				},
 			},
+		},
+	},
+	{
+		"chentoast/marks.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			default_mappings = true,
+			-- builtin marks shown in the gutter too (last insert, last change, etc.)
+			builtin_marks = { ".", "<", ">", "^" },
+			-- wrap around the buffer when jumping between marks
+			cyclic = true,
+			refresh_interval = 250,
+			sign_priority = { lower = 10, upper = 15, builtin = 8, bookmark = 20 },
+		},
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = true,
+		keys = {
+			{ "<m-h>", ":TmuxNavigateLeft<cr>", silent = true },
+			{ "<m-j>", ":TmuxNavigateDown<cr>", silent = true },
+			{ "<m-k>", ":TmuxNavigateUp<cr>", silent = true },
+			{ "<m-l>", ":TmuxNavigateRight<cr>", silent = true },
+			{ "<m-\\>", ":TmuxNavigatePrevious<cr>", silent = true },
 		},
 	},
 }
