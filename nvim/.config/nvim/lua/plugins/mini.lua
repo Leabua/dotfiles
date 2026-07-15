4d09956b1c5cc809c94edccf56291a279e0122af
Leabua Khomari <leabuakhomari@gmail.com>
2026-07-15 14:30:23 +0200

    mini nvim <CR> issue and trying mini-ai vaF

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,10 +1,18 @@
 return {
-	-- Better a/i text objects (function args, brackets, quotes, arguments, ...)
+	-- Better a/i text objects
 	{
 		"echasnovski/mini.ai",
 		event = "VeryLazy",
 		config = function()
-			require("mini.ai").setup()
+			local ai = require("mini.ai")
+			local gen_spec = ai.gen_spec
+
+			ai.setup({
+				custom_textobjects = {
+					-- Maps 'F' to the entire function definition via Treesitter
+					F = gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
+				},
+			})
 		end,
 	},
 
@@ -13,7 +21,9 @@ return {
 		"echasnovski/mini.pairs",
 		event = "InsertEnter",
 		config = function()
-			require("mini.pairs").setup()
+			require("mini.pairs").setup({
+				map_cr = true,
+			})
 		end,
 	},
 }
